103a9f2234449c8198c50f564ff7dca1d0179723
Jean-Marc INIKO <jminiko@users.noreply.github.com>
2016-01-10 04:17:34 +0100

    double decode (snipped)

--- grails-app/controllers/simplecms/ContentController.groovy
+++ grails-app/controllers/simplecms/ContentController.groovy
@@ -5,6 +5,6 @@ class ContentController {
     def index() { 
     
         def page_model = simplecms.Page.first()
-        render(view:"index.gsp",model:[content:page_model.content.decodeHTML().decodeHTML()],layout : null)
+        render(view:"index.gsp",model:[content:page_model.content.decodeHTML()],layout : null)
     }
 }
